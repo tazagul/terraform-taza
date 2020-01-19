9b6c9493aa39467507c413724e6d8fe1ea983e09
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-19 15:54:34 -0800

    added sg

--- sg.tf
+++ sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_ssh" { 
-  name        = "allow_ssh" 
+  name        = "allow_ssh." 
   description = "Allow SSH inbound traffic" 
   vpc_id      = "vpc-b392dbc9" 
 
