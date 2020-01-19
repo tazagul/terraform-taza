f2bd6e03a22a9ac78bef5be5c9082dad98490249
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-19 15:15:35 -0800

    added keyname

--- instance.tf
+++ instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" { 
   ami           = "ami-00068cd7555f543d5" 
   instance_type = "t2.micro" 
- 
-
+  associate_public_ip_address = "true" 
+  key_name = aws_key_pair.deployer.key_name 
 tags = { 
     Name = "HelloWorld" 
   } 
