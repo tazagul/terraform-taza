458faba9ba437270a49e570ea21de3c1a8b4d31e
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-19 15:59:43 -0800

    added sec group to an instance

--- instance.tf
+++ instance.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "web" {
   associate_public_ip_address = "true" 
   key_name = aws_key_pair.deployer.key_name 
   user_data = file("userdata_file")
+  security_group = ["allow_ssh."]
   tags = { 
     Name = "HelloWorld" 
   } 
