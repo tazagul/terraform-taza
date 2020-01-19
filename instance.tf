93f3367106d55bd569f84a7ffbf4ea3e1acdd9ed
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-19 15:34:34 -0800

    added userdata

--- instance.tf
+++ instance.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "web" { 
-  ami           = "ami-00068cd7555f543d5" 
+  ami = "ami-00068cd7555f543d5" 
   instance_type = "t2.micro" 
   associate_public_ip_address = "true" 
   key_name = aws_key_pair.deployer.key_name 
-tags = { 
+  user_data = file("userdata_file")
+  tags = { 
     Name = "HelloWorld" 
   } 
 } 
