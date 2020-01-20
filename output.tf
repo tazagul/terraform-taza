87991c068e3ba455fd1c96cfa1fe020a1f3383bc
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-20 13:48:20 -0800

    output

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "bucket_name" { 
     value = aws_s3_bucket.b.bucket 
 } 
+output "sec_group" { 
+    value = aws_security_group.allow_ssh.name
+} 
+output "key_name" { 
+    value = aws_key_pair.deployer.key_name
+} 
+output "username" { 
+    value = "some_username"
+} 
+output "password" { 
+    value = "password"
+} 
