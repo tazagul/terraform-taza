4eb6331b3bd0de4bf907c05e7ffec8b45035d606
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-20 14:01:23 -0800

    added zone id

---
+++ route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "www" { 
+  zone_id = "Z27HH3KW5RGYXX" 
+  name    = "www.tazagul.net" 
+  type    = "A" 
+  ttl     = "60" 
+  records = [aws_instance.web.public_ip] 
+} 
