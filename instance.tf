2d79c5c0beac7147f58726f31efbf3cad842a28e
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-20 17:08:54 -0800

    added tfvars

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" { 
   count = 10
-  ami = "ami-00068cd7555f543d5" 
+  ami = var.ami
   instance_type = "t2.micro" 
   associate_public_ip_address = "true" 
   key_name = aws_key_pair.deployer.key_name 
