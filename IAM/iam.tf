bd454198fdda49bc6ad5f9a5e39d405355c4d68a
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-21 00:40:17 -0800

    added role

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -13,3 +13,44 @@ resource "aws_iam_group_membership" "developers_team" {
   ] 
   group = aws_iam_group.developers.name 
 } 
+
+resource "aws_iam_policy" "policy" { 
+  name = "test_policy3" 
+  path = "/" 
+  description = "My test policy" 
+  policy = <<EOF 
+{
+"Version": "2012-10-17", 
+"Statement": [ 
+{ 
+"Sid": "VisualEditor0", 
+"Effect": "Allow", 
+"Action": "ec2:*", 
+"Resource": "*" 
+} 
+] 
+} 
+EOF
+} 
+
+
+
+
+resource "aws_iam_role" "test_role" { 
+name = "test_role" 
+assume_role_policy = <<EOF 
+{ 
+  "Version": "2012-10-17", 
+  "Statement": [ 
+{ 
+  "Action": "sts:AssumeRole", 
+  "Principal": { 
+  "Service": "ec2.amazonaws.com" 
+}, 
+  "Effect": "Allow", 
+  "Sid": "" 
+    } 
+  ]
+} 
+EOF 
+} 
