bdcaea8131268a92efb74e1b1d821d2f8ec2f1d6
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-20 16:41:09 -0800

    count line is deleted

--- instance.tf
+++ instance.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "web" { 
-  count = 10
   ami = "ami-00068cd7555f543d5" 
   instance_type = "t2.micro" 
   associate_public_ip_address = "true" 
@@ -7,11 +6,6 @@ resource "aws_instance" "web" {
   user_data = file("userdata_file")
   security_groups = ["allow_ssh"]
   tags = { 
-    Name = "HelloWorld${count.index +1}"
+    Name = "HelloWorld
   } 
 } 
-resource "aws_instance" "imported" {
- ami = "ami-00068cd7555f543d5" 
- key_name = aws_key_pair.deployer.key_name 
- security_groups = ["allow_ssh"]
-}
