fb8540159fcd3977329df4b5378cc13f613f21c5
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-20 16:18:55 -0800

    imported ec2

--- instance.tf
+++ instance.tf
@@ -10,3 +10,8 @@ resource "aws_instance" "web" {
     Name = "HelloWorld${count.index +1}"
   } 
 } 
+resource "aws_instance" "imported" {
+ ami = "ami-00068cd7555f543d5" 
+ key_name = aws_key_pair.deployer.key_name 
+ security_groups = ["allow_ssh"]
+}
