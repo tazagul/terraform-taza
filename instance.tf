8404356961e86b064642ebddad9c36ac34094677
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-20 16:50:00 -0800

    sdc

--- instance.tf
+++ instance.tf
@@ -7,6 +7,13 @@ resource "aws_instance" "web" {
   user_data = file("userdata_file")
   security_groups = ["allow_ssh"]
   tags = { 
-    Name = "HelloWorld
+    Name = "HelloWorld${count.index +1}
   } 
+} 
+
+resource "aws_instance" "imported" {
+  ami = "ami-00068cd7555f543d5"
+  key_name = aws_key_pair.deployer.key_name 
+  security_groups = ["allow_ssh"]
+  instance_type = "t2.micro" 
 } 
