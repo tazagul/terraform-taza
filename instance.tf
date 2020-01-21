811fb974527bd5822af80e9851ffda6c59dc0bc4
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-20 16:51:59 -0800

    ghv

--- instance.tf
+++ instance.tf
@@ -14,4 +14,5 @@ resource "aws_instance" "imported" {
  ami = "ami-00068cd7555f543d5" 
  key_name = aws_key_pair.deployer.key_name 
  security_groups = ["allow_ssh"]
+ instance_type = "t2.micro" 
 }
