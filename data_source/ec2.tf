42a193c49ebbbd085b1f33ee722d3a2a6fecca2e
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-21 18:28:33 -0800

    s

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,12 +1,12 @@
-provider "aws" { 
-  region = "us-east-2" 
+provider "aws" {
+    region = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners = ["099720109477"]
 } 
 
-data "aws_ami" "ubuntu" { 
-  most_recent = true 
-  owners = ["099720109477"] 
-} 
-
-output "ami" { 
-  value = "${data.aws_ami.ubuntu.id}" 
-} 
+output "ami" {
+    value = "${data.aws_ami.ubuntu.id}"
+}
