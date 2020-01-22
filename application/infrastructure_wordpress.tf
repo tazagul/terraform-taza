d9925f0cecb452628fb61e768c5fe8b6176899f9
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-21 18:05:59 -0800

    var

--- application/infrastructure_wordpress.tf
+++ application/infrastructure_wordpress.tf
@@ -6,7 +6,7 @@ module "wordpress1" {
 
   azs             = [var.azs1, var.azs2, var.azs3]
   private_subnets = [var.private_subnets1, var.private_subnets2,var.private_subnets3]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = [var.public_subnets1, var.public_subnets2, var.public_subnets3]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
--- application/variables.tf
+++ application/variables.tf
@@ -8,3 +8,6 @@ variable "azs3" {}
 variable "private_subnets1" {}
 variable "private_subnets2" {}
 variable "private_subnets3" {}
+variable "public_subnets1" {}
+variable "public_subnets2" {}
+variable "public_subnets3" {}
