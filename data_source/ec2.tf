48a03ae48f285d9510db0581392c6fff377e405e
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-21 18:24:57 -0800

    added new ec2

---
+++ data_source/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" { 
+  region = "us-east-2" 
+} 
+
+data "aws_ami" "ubuntu" { 
+  most_recent = true 
+  owners = ["099720109477"] 
+} 
+
+output "ami" { 
+  value = "${data.aws_ami.ubuntu.id}" 
+} 
