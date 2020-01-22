a2bdab92c531f2bbbe8d811a4146f81470cdd3e7
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-21 18:40:34 -0800

    instance added

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -10,3 +10,12 @@ data "aws_ami" "ubuntu" {
 output "ami" {
     value = "${data.aws_ami.ubuntu.id}"
 }
+resource "aws_instance" "web" { 
+  ami           = "${data.aws_ami.ubuntu.id}" 
+  instance_type = "t2.micro" 
+ 
+
+tags = { 
+    Name = "HelloWorld" 
+  } 
+} 
