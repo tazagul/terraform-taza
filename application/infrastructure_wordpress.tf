480d0a0815ab66c5f5fc346194fd80892a328336
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-21 16:41:48 -0800

    var

--- application/infrastructure_wordpress.tf
+++ application/infrastructure_wordpress.tf
@@ -5,7 +5,7 @@ module "wordpress1" {
   cidr = var.cidr
 
   azs             = [var.azs1, var.azs2, var.azs3]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = [var.private_subnets1, var.private_subnets3,var.private_subnets3]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
--- application/variables.tf
+++ application/variables.tf
@@ -5,3 +5,6 @@ variable "cidr" {}
 variable "azs1" {}
 variable "azs2" {}
 variable "azs3" {}
+variable " private_subnets1" {}
+variable " private_subnets2" {}
+variable " private_subnets3" {}
