bd74bace8e4e9a99b4efaeddd74ab7ffe62b030d
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-30 13:57:54 -0600

    s3

--- terraform-task/s3.tf
+++ terraform-task/s3.tf
@@ -7,16 +7,11 @@ resource "aws_s3_bucket" "public" {
   bucket = "tf-task-taza1"
   region = "us-east-1"
 
-  website {
-    index_document = "index.html"
-  }
+ 
 }
 
 resource "aws_s3_bucket" "secrets" {
   bucket = "tf-task-taza2"
   region = "us-east-2"
 
-  versioning {
-    enabled = true
-  }
 }
