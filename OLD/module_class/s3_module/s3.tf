03f018c43b4934b65aa93e49b43fb9245af19ad6
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-30 13:43:47 -0600

    2

--- OLD/module_class/s3_module/s3.tf
+++ OLD/module_class/s3_module/s3.tf
@@ -6,5 +6,16 @@ provider "aws"{
  
 module "bucket1" { 
   source = "./modules" 
-  bucketname = "modules-us-kumarovatazagul" 
+  bucketname = "modules-us-kumarovatazagul1" 
 }  
+
+provider "aws"{ 
+  region = "us-west-1" 
+
+} 
+
+ 
+module "bucket1" { 
+  source = "./modules" 
+  bucketname = "modules-us-kumarovatazagul2" 
+}
