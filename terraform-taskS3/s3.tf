b29dc1036c17c9f1c7f4c70b2f9d52f6563724a5
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-30 15:05:27 -0600

    s3

--- terraform-taskS3/s3.tf
+++ terraform-taskS3/s3.tf
@@ -4,9 +4,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "a" {
-  bucket  = "taza-task-tf4-s3"
+  bucket  = "taza-task-tf5-s3"
   region  = "us-east-1"
 }
+
 
 resource "aws_s3_bucket" "b" {
   bucket  = "taza-task-tf3-s3"
