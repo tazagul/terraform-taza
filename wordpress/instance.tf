c2541df385b1a03068676aa823d9d5b2c360143b
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-30 12:37:10 -0600

    -1

--- wordpress/instance.tf
+++ wordpress/instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami               = data.aws_ami.centos.id
   instance_type     = var.instance_type
   key_name          = aws_key_pair.bastion-wordpress.key_name
-  vpc_security_group_ids = [aws_security_group.ssh-1.id,aws_security_group.web-1.id]
+  vpc_security_group_ids = [aws_security_group.ssh.id,aws_security_group.web.id]
   
 
   
